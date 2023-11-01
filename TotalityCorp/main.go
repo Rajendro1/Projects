bd7d2d6fd74a54273e1119cd834e1b517a356529
Rajendro1 <rajandroprosad@gmail.com>
2023-11-01 19:24:46 +0530

    adding users route

--- TotalityCorp/main.go
+++ TotalityCorp/main.go
@@ -7,10 +7,12 @@ import (
 	"net"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 	userpb "main.go/userProto"
 )
@@ -18,10 +20,15 @@ import (
 // Mock database
 var mockDB = []userpb.User{
 	{Id: 1, Fname: "Steve", City: "LA", Phone: 1234567890, Height: 5.8, Married: true},
+	{Id: 2, Fname: "Rajendro", City: "Kolkata", Phone: 8250771252, Height: 5.9, Married: false},
+
 	// Add more users if needed
 }
 
-type server struct{}
+type server struct {
+	userpb.UnimplementedUserServiceServer
+	users []*userpb.User
+}
 
 func (s *server) GetUserById(ctx context.Context, req *userpb.UserRequest) (*userpb.User, error) {
 	id := req.GetId()
@@ -64,8 +71,9 @@ func startGRPCServer() {
 func startGinServer() {
 	r := gin.Default()
 
-	r.GET("/user/:id", func(c *gin.Context) {
-		idStr := c.Param("id")
+	r.GET("/user", func(c *gin.Context) {
+		// idStr := c.Param("id")
+		idStr := c.Request.FormValue("id")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
@@ -73,7 +81,7 @@ func startGinServer() {
 		}
 
 		// Connect to gRPC server
-		cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to gRPC server"})
 			return
@@ -93,12 +101,41 @@ func startGinServer() {
 
 		c.JSON(http.StatusOK, resp)
 	})
+	r.GET("/users", func(c *gin.Context) {
+		idsStr := c.Query("ids")
+		idStrs := strings.Split(idsStr, ",")
+		var ids []int32
+		for _, idStr := range idStrs {
+			id, err := strconv.Atoi(idStr)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+				return
+			}
+			ids = append(ids, int32(id))
+		}
 
-	// Implement route for fetching multiple users
+		// Connect to gRPC server
+		cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to gRPC server"})
+			return
+		}
+		defer cc.Close()
+
+		client := userpb.NewUserServiceClient(cc)
+		resp, err := client.GetUsersByIds(context.Background(), &userpb.UsersRequest{Ids: ids})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch user details"})
+			return
+		}
+
+		c.JSON(http.StatusOK, resp)
+	})
 
 	r.Run(":8080") // Start HTTP server on port 8080
 }
 
+// protoc --go_out=. --go-grpc_out=. user.proto
 func main() {
 	go startGRPCServer()
 	startGinServer()
