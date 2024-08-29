f077c57e2fd0f4561de691402cba3a17b6b44bf6
Rajendro1 <rajandroprosad@gmail.com>
2024-08-29 22:47:50 +0530

    testing

--- GO_CURL/struct.go
+++ GO_CURL/struct.go
@@ -1,16 +1,1 @@
 package main
-
-type ParsedCurl struct {
-	Method       string
-	Headers      map[string]string
-	Body         string
-	URL          string
-	ContentType  string
-	AuthType     string
-	AuthToken    string
-	AuthUsername string
-	AuthPassword string
-	AuthParams   map[string]string
-	UserAgent    string
-	DataType     string
-}
